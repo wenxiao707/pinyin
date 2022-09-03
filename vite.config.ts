f2396f1f6224791ba84f4f8104a994cdf39b1b78
wenxiao707 <110232649+wenxiao707@users.noreply.github.com>
2022-09-03 20:16:08 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import vitePluginImp from 'vite-plugin-imp';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import vitApp from '@vitjs/vit';pinyin
+import vitApp from '@vitjs/vit';
 
 // https://vitejs.dev/config/
 export default defineConfig({
